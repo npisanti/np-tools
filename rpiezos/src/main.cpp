7c4ca2236f79e45e7cf9e5c145bea83f72c556ff
Nicola Pisanti <nicola@npisanti.com>
2019-10-26 12:16:21 +0200

    adds warning for no argument given to rpiezos

--- rpiezos/src/main.cpp
+++ rpiezos/src/main.cpp
@@ -45,5 +45,7 @@ int main( int argc, char *argv[] ){
             ofSetupOpenGL(&window, 256,256,OF_WINDOW);  
             ofRunApp( app );            
         }        
+    }else{
+        std::cout<<"[rpiezos] no input file given! you should supply a .json config path\n";
     }
 }
