c67a491c6f1895c8ad7890053bc1d23ba5005542
Nicola Pisanti <nicola@npisanti.com>
2019-11-21 13:29:06 +0100

    default masterbus rev to -24dB

--- masterbus/src/ofApp.cpp
+++ masterbus/src/ofApp.cpp
@@ -58,7 +58,7 @@ void ofApp::setup(){
     limiter.thresholdControl.getOFParameterInt() = -9;
     limiter.makeup.getOFParameterInt() = 9;
     limiter.clipThreshold.getOFParameterInt() = -2;
-    reverb.gain.getOFParameterInt() = -18;
+    reverb.gain.getOFParameterInt() = -24;
     
     // JSON settings --------
     if( path!="" ){
